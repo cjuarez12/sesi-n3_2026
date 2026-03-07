602412cdcef04b95d38cd47fa908e2727f1f0896
cjuarez12 <32677214+cjuarez12@users.noreply.github.com>
2026-03-06 22:11:48 -0600

    Create Estudiante.java

---
+++ Estudiante.java
@@ -0,0 +1,33 @@
+public class Estudiante {
+
+    private String nombre;
+    private int edad;
+    private double promedio;
+
+    // Getter y Setter de nombre
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    // Getter y Setter de edad
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    // Getter y Setter de promedio
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+}
